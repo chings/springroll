f299a707e568151df23d63d4530a0fc08b7d22d6
ching <Sdv123123>
2018-07-17 15:10:56 +0800

    favicon.ico

--- springroll-framework/springroll-framework-connector/src/main/resource/public/js/springroll.js
+++ springroll-framework/springroll-framework-connector/src/main/resource/public/js/springroll.js
@@ -119,13 +119,3 @@ var SpringRollConnection = function (url, onOpen, onError, onClose) {
     };
 
 };
-
-var randomString = function (len, charSet) {
-    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var randomString = '';
-    for (var i = 0; i < len; i++) {
-        var randomPoz = Math.floor(Math.random() * charSet.length);
-        randomString += charSet.substring(randomPoz, randomPoz + 1);
-    }
-    return randomString;
-};
